1df2a05fbea10f167a180ecf4dff628daf01bb5c
Kieran <hawkinski2019@gmail.com>
2025-08-21 19:47:33 -0700

    feat: Add conftest.py and basic pytest fixtures

--- backend/app/schemas/user.py
+++ backend/app/schemas/user.py
@@ -1,17 +1,18 @@
 from pydantic import BaseModel, ConfigDict, EmailStr
+from typing import Optional
 from datetime import datetime
 
 
 class UserBase(BaseModel):
     username: str
     email: EmailStr
-    first_name: str
-    sex: str
-    age: int
-    weight: float
-    height: int
-    goal: str
-    settings: dict
+    first_name: Optional[str] = None
+    sex: Optional[str] = None
+    age: Optional[int] = None
+    weight: Optional[float] = None
+    height: Optional[int] = None
+    goal: Optional[str] = None
+    settings: Optional[dict] = None
 
 class UserCreate(UserBase):
     password: str
---
+++ backend/tests/conftest.py
@@ -0,0 +1,78 @@
+import pytest
+from fastapi.testclient import TestClient
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from app.main import app
+from app.core.config import settings
+from app.models import models
+from app.core.db import get_db
+from app.core.oauth2 import create_access_token
+
+
+"""
+==============================================================================
+---- Running Tests ----
+
+To run all tests, in AI-Fitness-Tracker/backend, run:
+    python -m pytest -v -s
+
+To run all tests with warnings disabled, in AI-Fitness-Tracker/backend, run:
+    python -m pytest -v -s --disable-warnings
+
+==============================================================================
+"""
+
+TEST_DATABASE_URL = (
+    f"postgresql://"
+    f"{settings.database_username}:"
+    f"{settings.database_password}@"
+    f"{settings.database_hostname}:"
+    f"{settings.database_port}/"
+    f"{settings.database_name}_test"
+)
+
+engine = create_engine(TEST_DATABASE_URL)
+
+TestingSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+@pytest.fixture
+def session():
+    models.Base.metadata.drop_all(bind=engine)
+    models.Base.metadata.create_all(bind=engine)
+
+    db = TestingSessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+@pytest.fixture
+def client(session):
+    def override_get_db():
+        yield session
+
+    app.dependency_overrides[get_db] = override_get_db
+
+    yield TestClient(app)
+
+@pytest.fixture
+def user(client):
+    user_data = {"username": "username",
+                 "email": "email@gmail.com",
+                 "password": "password"}
+
+    res = client.post("/users/", json=user_data)
+
+    new_user = res.json()
+    new_user["password"] = user_data["password"]
+    
+    return new_user
+
+@pytest.fixture
+def token(user):
+    return create_access_token({"user_id": user["id"]})
+
+@pytest.fixture
+def authorized_client(client, token):
+    client.headers = {"Authorization": f"Bearer {token}"}
+    return client
