44f4c9fffbf11c66c6f4b98be9cdd0cb5ba5ba7c
Kieran <hawkinski2019@gmail.com>
2025-12-19 23:57:17 -0800

    fix: Update the test_mood_logs tests
    Update the test_mood_logs tests now that the /mood-logs endpoint has
    changed.

--- backend/tests/integration/test_mood_logs.py
+++ backend/tests/integration/test_mood_logs.py
@@ -106,25 +106,25 @@ def test_mood_log_not_found(authorized_client, mood_logs, session):
     ]
 )
 def test_update_mood_log(authorized_client, mood_logs, data, status_code):
-    res = authorized_client.put(f"/api/mood-logs/{mood_logs[0].id}", json=data)
+    res = authorized_client.patch(f"/api/mood-logs/{mood_logs[0].id}", json=data)
     assert res.status_code == status_code
 
 def test_update_mood_log_invalid(authorized_client, mood_logs):
-    res = authorized_client.put(f"/api/mood-logs/{mood_logs[0].id}", json={"log_date": ""})
+    res = authorized_client.patch(f"/api/mood-logs/{mood_logs[0].id}", json={"mood_score": []})
     assert res.status_code == 422
 
 def test_update_mood_log_unauthorized(client, mood_logs):
-    res = client.put(f"/api/mood-logs/{mood_logs[0].id}", json={"log_date": datetime.now().isoformat()})
+    res = client.patch(f"/api/mood-logs/{mood_logs[0].id}", json={"log_date": datetime.now().isoformat()})
     assert res.status_code == 401
 
 def test_update_mood_log_not_owner(authorized_client, mood_logs):
-    res = authorized_client.put(f"/api/mood-logs/{mood_logs[2].id}", json={"log_date": datetime.now().isoformat()})
+    res = authorized_client.patch(f"/api/mood-logs/{mood_logs[2].id}", json={"log_date": datetime.now().isoformat()})
     assert res.status_code == 404
 
 def test_update_mood_log_not_found(authorized_client, mood_logs, session):
     max_id = session.query(MoodLog.id).order_by(MoodLog.id.desc()).first()[0]
     non_existent_id = max_id + 1000
-    res = authorized_client.put(f"/api/mood-logs/{non_existent_id}", json={"log_date": datetime.now().isoformat()})
+    res = authorized_client.patch(f"/api/mood-logs/{non_existent_id}", json={"log_date": datetime.now().isoformat()})
     assert res.status_code == 404
 
 # ----------------------------------------------------------------------------
